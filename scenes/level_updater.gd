dd29e3bb32e8710e246329558fe336b9339e4be7
ansh33430w <ansh.nothing.in@gmail.com>
2026-07-15 01:27:23 +0530

    added level 3 and completed option menu

--- scenes/level_updater.gd
+++ scenes/level_updater.gd
@@ -5,16 +5,7 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		var curpath =  get_tree().current_scene.scene_file_path
-		var regex   = RegEx.new(
-			
-		)
-		regex.compile("level_(\\d+)")
-		var result = regex.search(curpath
-		)
-		if result:
-			var nxtlevel = int(result.get_string(1))
-			var nextpath = curpath.replace(
-				"level_d%" % nxtlevel ,
-				"level_d% " % (nxtlevel+1)
-			)
+		var cur_scene = get_tree().current_scene.scene_file_path
+		var nxtlevel = cur_scene.to_int() + 1 
+		var nxtscene = "res://scenes/levels/level_" + str(nxtlevel) + ".tscn"
+		get_tree().change_scene_to_file(nxtscene)
--- scripts/options.gd
+++ scripts/options.gd
@@ -5,3 +5,14 @@ extends Control
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/mainmenu.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/LEVELS/level_1.tscn")
+	
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/LEVELS/level_2.tscn")
+
+func _on_button_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/LEVELS/level_3.tscn")
